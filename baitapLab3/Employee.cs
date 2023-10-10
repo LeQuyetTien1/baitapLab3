622305f1243f62f5734c21b3159f8ac9b8def706
Admin <Admin@ADMIN>
2023-10-10 16:11:15 +0700

    Add project files.

---
+++ baitapLab3/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitapLab3
+{
+    internal class Employee
+    {
+        private int id;
+        private string name;
+        private int yearOfBirth;
+        private double salaryLevel;
+        private double basicSalary;
+
+        public Employee() { }
+        public Employee(int id, string name, int yearOfBirth, double salaryLevel, double basicSalary)
+        {
+            this.id = id;
+            this.name = name;
+            this.yearOfBirth = yearOfBirth;
+            this.salaryLevel = salaryLevel;
+            this.basicSalary = basicSalary;
+        }
+
+        public int Id { get { return id; } set { id = value; } }
+        public string Name { get { return name; } set { name = value; } }
+        public int YearOfBirth { get { return yearOfBirth; } set { yearOfBirth = value; } }
+        public double SalaryLevel { get {  return salaryLevel; } set {  salaryLevel = value; } }
+        public double BasicSalary { get {  return basicSalary; } set {  basicSalary = value; } }
+
+        public double GetIncome()
+        {
+            return (salaryLevel * basicSalary);
+        }
+        public void Display()
+        {
+            Console.WriteLine("---Thong tin---");
+            Console.WriteLine("Dinh danh: " + id);
+            Console.WriteLine("Ten: " + name);
+            Console.WriteLine("Nam sinh: " + yearOfBirth);
+            Console.WriteLine("Bac luong: " + salaryLevel);
+            Console.WriteLine("Luong co ban: " + basicSalary);
+        }
+    }
+}
---
+++ baitapLab3/Program.cs
@@ -0,0 +1,23 @@
+﻿using baitapLab3;
+using System;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Employee ep1 = new Employee(1, "Tien", 1997, 2.1, 5000);
+        Employee ep2 = new Employee { Id = 2, Name = "Linh", YearOfBirth = 1997, SalaryLevel = 2.5, BasicSalary = 5000 };
+        Employee ep3 = new Employee();
+        ep3.Id = 3;
+        ep3.Name = "Tan";
+        ep3.YearOfBirth = 2003;
+        ep3.SalaryLevel = 3;
+        ep3.BasicSalary = 6000;
+
+        ep1.Display();
+        Console.WriteLine("Thu nhap cua nhan vien la: " + ep1.GetIncome());
+        ep2.Display();
+        Console.WriteLine("Thu nhap cua nhan vien la: " + ep2.GetIncome());
+        ep3.Display();
+        Console.WriteLine("Thu nhap cua nhan vien la: " + ep3.GetIncome());
+    }
+}
